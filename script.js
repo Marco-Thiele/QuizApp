49a52f4cc0f7021ddb23d95f8fa0057855819c01
Marco-Thiele <marcothiele1.1@gmx.de>
2023-02-18 17:52:41 +0100

    card

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        'question': 'Wer hat HTML erfunden',
+        'answer1': 'Robbie Williams',
+        'answer2': 'Lady Gaga',
+        'answer3': 'Tim Berners-Lee',
+        'answer4': 'Justin Bieber',
+        'right_answer': 3
+    },
+    {
+        'question': 'Wie nennt der Engländer seine Vermieterin',
+        'answer1': 'landlady',
+        'answer2': 'villagequeen',
+        'answer3': 'citymadam',
+        'answer4': 'townmiss',
+        'right_answer': 1
+    },
+    {
+        'question': 'Welche Videospielreihe wird nach fast 30 Jahren ab 2023 nicht mehr von EA SPORTS unter diesem Namen weitergeführt',
+        'answer1': 'Grand Theft Auto',
+        'answer2': 'Die Sims',
+        'answer3': 'FIFA',
+        'answer4': 'Super Mario Kart',
+        'right_answer': 3
+    },
+    {
+        'question': 'Was dient nicht zur Verhütung',
+        'answer1': 'Pille',
+        'answer2': 'Kondom',
+        'answer3': 'Coitus interruptus',
+        'answer4': 'Glücksspirale',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wer kann vielleicht schwimmen, aber nicht fliegen',
+        'answer1': 'Stockenten',
+        'answer2': 'Pfeifenten',
+        'answer3': 'Krickenten',
+        'answer4': 'Studenten',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wie viel Meter legt man zurück, während man im Auto bei 120 km/h für drei Sekunden auf sein Handy schaut',
+        'answer1': 'rund 100 Meter',
+        'answer2': 'etwa 50 Meter',
+        'answer3': 'knapp 30 Meter',
+        'answer4': 'ungefähr 15 Meter',
+        'right_answer': 1
+    },
+    {
+        'question': 'Was kommt in Ostasien häufig auf den Tisch',
+        'answer1': 'Sonicht',
+        'answer2': 'Soja',
+        'answer3': 'Soschoneher',
+        'answer4': 'Sovielleicht',
+        'right_answer': 2
+    },
+]
