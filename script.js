dd2b812a6d17612ecd0ef301c09c9fd156765b46
Marco-Thiele <marcothiele1.1@gmx.de>
2023-02-21 10:52:57 +0100

    audio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let questions = [
     {
-        'question': 'Wer hat HTML erfunden',
+        'question': 'Wer hat HTML erfunden?',
         'answer1': 'Robbie Williams',
         'answer2': 'Lady Gaga',
         'answer3': 'Tim Berners-Lee',
@@ -8,7 +8,7 @@ let questions = [
         'right_answer': 3
     },
     {
-        'question': 'Wie nennt der Engländer seine Vermieterin',
+        'question': 'Wie nennt der Engländer seine Vermieterin?',
         'answer1': 'landlady',
         'answer2': 'villagequeen',
         'answer3': 'citymadam',
@@ -16,7 +16,7 @@ let questions = [
         'right_answer': 1
     },
     {
-        'question': 'Welche Videospielreihe wird nach fast 30 Jahren ab 2023 nicht mehr von EA SPORTS unter diesem Namen weitergeführt',
+        'question': 'Welche Videospielreihe wird nach fast 30 Jahren ab 2023 nicht mehr von EA SPORTS unter diesem Namen weitergeführt?',
         'answer1': 'Grand Theft Auto',
         'answer2': 'Die Sims',
         'answer3': 'FIFA',
@@ -24,7 +24,7 @@ let questions = [
         'right_answer': 3
     },
     {
-        'question': 'Was dient nicht zur Verhütung',
+        'question': 'Was dient nicht zur Verhütung?',
         'answer1': 'Pille',
         'answer2': 'Kondom',
         'answer3': 'Coitus interruptus',
@@ -32,7 +32,7 @@ let questions = [
         'right_answer': 4
     },
     {
-        'question': 'Wer kann vielleicht schwimmen, aber nicht fliegen',
+        'question': 'Wer kann vielleicht schwimmen, aber nicht fliegen?',
         'answer1': 'Stockenten',
         'answer2': 'Pfeifenten',
         'answer3': 'Krickenten',
@@ -40,7 +40,7 @@ let questions = [
         'right_answer': 4
     },
     {
-        'question': 'Wie viel Meter legt man zurück, während man im Auto bei 120 km/h für drei Sekunden auf sein Handy schaut',
+        'question': 'Wie viel Meter legt man zurück, während man im Auto bei 120 km/h für drei Sekunden auf sein Handy schaut?',
         'answer1': 'rund 100 Meter',
         'answer2': 'etwa 50 Meter',
         'answer3': 'knapp 30 Meter',
@@ -48,7 +48,7 @@ let questions = [
         'right_answer': 1
     },
     {
-        'question': 'Was kommt in Ostasien häufig auf den Tisch',
+        'question': 'Was kommt in Ostasien häufig auf den Tisch?',
         'answer1': 'Sonicht',
         'answer2': 'Soja',
         'answer3': 'Soschoneher',
@@ -56,3 +56,95 @@ let questions = [
         'right_answer': 2
     },
 ]
+
+
+
+let images = ['/img/it-html.jpg', '/img/makler.png', '/img/games.png', '/img/verhütung.jpg', '/img/ente.png', '/img/tacho.png', '/img/sushi.png']
+
+let currentQuestion = 0;
+let rightQuestion = 0;
+
+function init() {
+    document.getElementById('allQuestion').innerHTML = questions.length;
+
+    showquestion()
+
+}
+
+
+function showquestion() {
+
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endscreen').style = '';
+        document.getElementById('questionbody').style = 'display: none';
+        endscreenNumber()
+        document.getElementById('picture').style = 'display: none';
+        document.getElementById('progressbar').parentNode.style = 'display: none';
+    } else {
+        let percent = currentQuestion / questions.length;
+        percent = Math.round(percent * 100);
+        document.getElementById('progressbar').innerHTML = `${percent} %`;
+        document.getElementById('progressbar').style = `width : ${percent}%`;
+
+        let question = questions[currentQuestion];
+        let image = images[currentQuestion];
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer1'];
+        document.getElementById('answer_2').innerHTML = question['answer2'];
+        document.getElementById('answer_3').innerHTML = question['answer3'];
+        document.getElementById('answer_4').innerHTML = question['answer4'];
+        document.getElementById('picture').src = image
+    }
+}
+
+
+
+function answer(selection) {
+    let right_answer = questions[currentQuestion]['right_answer'];
+    selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${right_answer}`;
+    if (selectedQuestionNumber == right_answer) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightQuestion++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    resetAnswerButtons()
+    showquestion()
+    document.getElementById('next-button').disabled = true;
+}
+
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success')
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success')
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success')
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success')
+}
+
+function endscreenNumber() {
+    document.getElementById('endscreenQuestionNumber').innerHTML = questions.length;
+    document.getElementById('rightQuestion').innerHTML = rightQuestion;
+}
+
+function restartGame() {
+    document.getElementById('questionbody').style = '';
+    document.getElementById('picture').style = '';
+    document.getElementById('progressbar').parentNode.style = '';
+    document.getElementById('endscreen').style = 'display: none';
+    currentQuestion = 0;
+    rightQuestion = 0;
+    init()
+}
+
